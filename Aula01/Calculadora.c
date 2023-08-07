3c95af003cfac18188c14f10cb2e1b634607e6e7
Victor S. Camargo <victorscamargo51@gmail.com>
2023-08-07 12:09:13 -0300

    Atualização dos tipos das variáveis que são usadas de forma algébrica do tipo int para double

--- Aula01/Calculadora.c
+++ Aula01/Calculadora.c
@@ -10,34 +10,34 @@ int main() {
 
     printf("Digite o primeiro número\n");
 
-    int numero1;
-    int numero2;
+    double numero1;
+    double numero2;
     int variavel;
 
-    scanf("%d", &numero1);
+    scanf("%lf", &numero1);
     printf("Digite o segundo número: \n");
-    scanf("%d", &numero2);
+    scanf("%lf", &numero2);
 
     printf("Digite 1 para somar \n Digite 2 para subtrair \n Digite 3 para Multiplicar \n Digite 4 para dividir \n");
     scanf("%d", &variavel);
 
-    int resultado;  
+    double resultado;  
 
     if (variavel == 1) {
         resultado = numero1 + numero2;
-        printf("O resultado da soma dos números %d e %d é: %d", numero1, numero2, resultado);
+        printf("O resultado da soma dos números %lf e %lf é: %lf", numero1, numero2, resultado);
 
     } else if (variavel == 2) {
         resultado = numero1 - numero2;
-        printf("O resultado da subtração dos números %d e %d é: %d", numero1, numero2, resultado);
+        printf("O resultado da soma dos números %lf e %lf é: %lf", numero1, numero2, resultado);
 
     } else if (variavel == 3) {
         resultado = numero1 * numero2;
-        printf("O resultado da multiplicação dos números %d e %d é: %d", numero1, numero2, resultado);
+        printf("O resultado da multiplicação dos números %lf e %lf é: %lf", numero1, numero2, resultado);
 
     } else if (variavel == 4) {
-        resultado = numero1 / numero2;
-        printf("O resultado da divisão dos números %d e %d é: %d", numero1, numero2, resultado);
+        resultado = (numero1 / numero2);
+        printf("O resultado da divisão dos números %lf e %lf é: %lf", numero1, numero2, resultado);
     } else {
         printf("Você não digitou nenhuma das opções");
     }
